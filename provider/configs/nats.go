b9b655ae595b4e7495097701fb955bbc197013a5
artaasadi <arta-c@kaytu.io>
2024-11-09 01:51:35 +0100

    fix: change credentials types

--- provider/configs/nats.go
+++ provider/configs/nats.go
@@ -3,7 +3,7 @@ package configs
 import "github.com/opengovern/opengovernance/services/integration/integration-type/entra-id-directory/configs"
 
 const (
-	StreamName           = configs.ConsumerGroup
+	StreamName           = configs.StreamName
 	JobQueueTopic        = configs.JobQueueTopic
 	ConsumerGroup        = configs.ConsumerGroup
 	JobQueueTopicManuals = configs.JobQueueTopicManuals
