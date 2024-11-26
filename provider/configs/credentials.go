b22c23a0be2527e1f3dc95d179b020272da63b90
artaasadi <arta-c@kaytu.io>
2024-11-26 19:58:41 +0100

    fix: change opengovernance to opencomply

--- provider/configs/credentials.go
+++ provider/configs/credentials.go
@@ -1,6 +1,6 @@
 package configs
 
-import "github.com/opengovern/opengovernance/services/integration/integration-type/entra-id-directory/configs"
+import "github.com/opengovern/opencomply/services/integration/integration-type/entra-id-directory/configs"
 
 type IntegrationCredentials struct {
 	configs.IntegrationCredentials
