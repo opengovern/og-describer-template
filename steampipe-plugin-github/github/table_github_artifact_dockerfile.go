2b8a304a683a3f9e25135cf0957305fcd945d755
artaasadi <arta-c@kaytu.io>
2024-12-19 15:09:02 +0100

    fix: containerpackage and dockerfile table mapping

--- steampipe-plugin-github/github/table_github_artifact_dockerfile.go
+++ steampipe-plugin-github/github/table_github_artifact_dockerfile.go
@@ -4,6 +4,7 @@ import (
 	opengovernance "github.com/opengovern/og-describer-github/pkg/sdk/es"
 	"github.com/turbot/steampipe-plugin-sdk/v5/grpc/proto"
 	"github.com/turbot/steampipe-plugin-sdk/v5/plugin"
+	"github.com/turbot/steampipe-plugin-sdk/v5/plugin/transform"
 )
 
 func tableGitHubArtifactDockerFile() *plugin.Table {
@@ -16,18 +17,58 @@ func tableGitHubArtifactDockerFile() *plugin.Table {
 			KeyColumns: plugin.AllColumns([]string{"sha"}),
 			Hydrate:    opengovernance.GetArtifactDockerFile,
 		},
-		Columns: []*plugin.Column{
+		Columns: commonColumns([]*plugin.Column{
 			// Basic details columns
-			{Name: "sha", Type: proto.ColumnType_STRING, Description: "SHA hash of the Dockerfile."},
-			{Name: "name", Type: proto.ColumnType_STRING, Description: "Name of the Dockerfile."},
-			{Name: "path", Type: proto.ColumnType_STRING, Description: "Path to the Dockerfile in the repository."},
-			{Name: "last_updated_at", Type: proto.ColumnType_TIMESTAMP, Description: "Timestamp when the Dockerfile was last updated."},
-			{Name: "git_url", Type: proto.ColumnType_STRING, Description: "Git URL where the Dockerfile can be accessed."},
-			{Name: "html_url", Type: proto.ColumnType_STRING, Description: "HTML URL where the Dockerfile can be accessed."},
-			{Name: "uri", Type: proto.ColumnType_STRING, Description: "Unique URI for the Dockerfile."},
-			{Name: "dockerfile_content", Type: proto.ColumnType_STRING, Description: "Content of the Dockerfile."},
-			{Name: "dockerfile_content_base64", Type: proto.ColumnType_STRING, Description: "Base64-encoded content of the Dockerfile."},
-			{Name: "repository", Type: proto.ColumnType_JSON, Description: "Repository metadata associated with the Dockerfile."},
-		},
+			{
+				Name:        "sha",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.Sha"),
+				Description: "SHA hash of the Dockerfile."},
+			{
+				Name:        "name",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.Name"),
+				Description: "Name of the Dockerfile."},
+			{
+				Name:        "path",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.Path"),
+				Description: "Path to the Dockerfile in the repository."},
+			{
+				Name:        "last_updated_at",
+				Type:        proto.ColumnType_TIMESTAMP,
+				Transform:   transform.FromField("Description.LastUpdatedAt"),
+				Description: "Timestamp when the Dockerfile was last updated."},
+			{
+				Name:        "git_url",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.GitURL"),
+				Description: "Git URL where the Dockerfile can be accessed."},
+			{
+				Name:        "html_url",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.HTMLURL"),
+				Description: "HTML URL where the Dockerfile can be accessed."},
+			{
+				Name:        "uri",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.URI"),
+				Description: "Unique URI for the Dockerfile."},
+			{
+				Name:        "dockerfile_content",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.DockerfileContent"),
+				Description: "Content of the Dockerfile."},
+			{
+				Name:        "dockerfile_content_base64",
+				Type:        proto.ColumnType_STRING,
+				Transform:   transform.FromField("Description.DockerfileContentBase64"),
+				Description: "Base64-encoded content of the Dockerfile."},
+			{
+				Name:        "repository",
+				Type:        proto.ColumnType_JSON,
+				Transform:   transform.FromField("Description.Repository"),
+				Description: "Repository metadata associated with the Dockerfile."},
+		}),
 	}
 }
