5c2c924365c08c90363085ccc0aaba336ae7f77c
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2024-12-25 04:12:47 +0330

    feat: update docker file

--- provider/model/model.go
+++ provider/model/model.go
@@ -1348,15 +1348,15 @@ type CommitResponse struct {
 }
 
 type ArtifactDockerFileDescription struct {
-	Sha                     *string                 `json:"sha"`
-	Name                    *string                 `json:"name"`
-	Path                    *string                 `json:"path"`
-	LastUpdatedAt           *string                 `json:"last_updated_at"`
-	GitURL                  *string                 `json:"git_url"`
-	HTMLURL                 *string                 `json:"html_url"`
-	URI                     *string                 `json:"uri"` // Unique identifier
-	DockerfileContent       string                 `json:"dockerfile_content"`
-	DockerfileContentBase64 *string                 `json:"dockerfile_content_base64"`
-	Repository              map[string]interface{} `json:"repository"`
-	Images                  []string               `json:"images"` // New field to store extracted base images
+	Sha                     *string                 
+	Name                    *string                 
+	Path                    *string                 
+	LastUpdatedAt           *string                 
+	GitURL                  *string                 
+	HTMLURL                 *string                 
+	URI                     *string                  // Unique identifier
+	DockerfileContent       string                 
+	DockerfileContentBase64 *string                
+	Repository              map[string]interface{} 
+	Images                  []string                // New field to store extracted base images
 }
