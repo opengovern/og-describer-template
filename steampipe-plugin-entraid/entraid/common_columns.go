50eddd35696d5c671c0004e6968023024bb26a30
artaasadi <arta-c@kaytu.io>
2024-12-28 20:11:18 +0100

    fix: add platform_integration_id to common columns

--- steampipe-plugin-entraid/entraid/common_columns.go
+++ steampipe-plugin-entraid/entraid/common_columns.go
@@ -22,7 +22,7 @@ func commonOGColumns() []*plugin.Column {
 			Transform: transform.FromField("Metadata.SubscriptionID"),
 		},
 		{
-			Name:        "platform_account_id",
+			Name:        "platform_integration_id",
 			Type:        proto.ColumnType_STRING,
 			Description: "The Platform Account ID in which the resource is located.",
 			Transform:   transform.FromField("IntegrationID"),
