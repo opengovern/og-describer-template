04541cba5712a3fb5123bfb0d0d8724865301e89
Mahan Zendedel DH <mahanmmi@gmail.com>
2025-01-15 16:03:25 +0400

    fix: pointer

--- plugin/integration/integration/github_account.go
+++ plugin/integration/integration/github_account.go
@@ -84,13 +84,13 @@ func (i *Integration) DiscoverIntegrations(jsonData []byte) ([]models.Integratio
 	return integrations, nil
 }
 
-func (i *Integration) GetResourceTypesByLabels(labels map[string]string) (map[string]*interfaces.ResourceTypeConfiguration, error) {
-	resourceTypesMap := make(map[string]*interfaces.ResourceTypeConfiguration)
+func (i *Integration) GetResourceTypesByLabels(labels map[string]string) (map[string]interfaces.ResourceTypeConfiguration, error) {
+	resourceTypesMap := make(map[string]interfaces.ResourceTypeConfiguration)
 	for _, resourceType := range configs.ResourceTypesList {
 		if v, ok := configs.ResourceTypeConfigs[resourceType]; ok {
-			resourceTypesMap[resourceType] = v
+			resourceTypesMap[resourceType] = *v
 		} else {
-			resourceTypesMap[resourceType] = nil
+			resourceTypesMap[resourceType] = interfaces.ResourceTypeConfiguration{}
 		}
 	}
 	return resourceTypesMap, nil
