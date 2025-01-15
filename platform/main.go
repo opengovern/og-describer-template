c61959fe0cb787f484fac3afa33c1438bb053d5e
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-01-16 01:51:41 +0330

    fix: builds

--- platform/main.go
+++ platform/main.go
@@ -3,9 +3,9 @@ package main
 import (
 	"github.com/hashicorp/go-hclog"
 	"github.com/hashicorp/go-plugin"
-	"github.com/opengovern/opencomply/services/integration/integration-type/interfaces"
 	"os"
 )
+
 
 func main() {
 	i := Integration{}
@@ -16,11 +16,11 @@ func main() {
 	})
 
 	var pluginMap = map[string]plugin.Plugin{
-		IntegrationTypeGithubAccount.String(): &interfaces.IntegrationTypePlugin{Impl: &i},
+		IntegrationTypeGithubAccount.String(): &IntegrationTypePlugin{Impl: &i},
 	}
 
 	plugin.Serve(&plugin.ServeConfig{
-		HandshakeConfig: interfaces.HandshakeConfig,
+		HandshakeConfig: HandshakeConfig,
 		Plugins:         pluginMap,
 		Logger:          logger,
 	})
