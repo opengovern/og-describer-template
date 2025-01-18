f84577f0d6f5c92686769ff631a68714a131e28f
Mohamad Choupan <mohamadkhoee@aut.ac.ir>
2025-01-19 02:55:30 +0330

    feat:remove all files

--- provider/configs/credentials.go
+++
@@ -1,7 +0,0 @@
-package configs
-
-import "github.com/opengovern/opencomply/services/integration/integration-type/entra-id-directory/configs"
-
-type IntegrationCredentials struct {
-	configs.IntegrationCredentials
-}
